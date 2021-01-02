9908940ef256d47141fcf0e3c4b89213ea5e4c37
bungaepyo <bungaepyo@gmail.com>
2021-01-02 17:41:43 -0500

    loading logic

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading/>;
+export default class extends React.Component {
+
+    state = {
+        isLoading: true,
+    };
+
+    getLocation = async() => {
+        try{
+            const response  = await Location.requestPermissionsAsync();
+            const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
+            this.setState({isLoading: false});
+            //Send to API and get Weather
+        } catch (error) {
+            Alert.alert("Location Permission Denied.", "Please enable location.");
+        }
+    }
+
+    componentDidMount(){
+        this,this.getLocation();
+    }
+
+    render(){
+        const {isLoading} = this.state;
+        return isLoading ? <Loading/> : null;
+    }
 }
