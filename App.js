2dd8e1402e32ec811f6b7bf4a097dfec6619eeac
bungaepyo <bungaepyo@gmail.com>
2021-01-03 20:55:24 -0500

    add weather + icons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Alert} from "react-native";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import Weather from "./Weather";
 import * as Location from "expo-location";
@@ -14,16 +14,27 @@ export default class extends React.Component {
     };
 
     getWeather = async (latitude, longitude) => {
-        const { data } = await axios.get(
-          `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+        const { 
+            data: { 
+                main: { 
+                    temp 
+                },
+                weather
+            } 
+        } = await axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
         );
-        this.setState({isLoading: false, temp: data.main.temp});
+        this.setState({
+            isLoading: false,
+            temp,
+            condition: weather[0].main
+        });
     };
 
-    getLocation = async() => {
-        try{
-            const response  = await Location.requestPermissionsAsync();
-            const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
+    getLocation = async () => {
+        try {
+            const response = await Location.requestPermissionsAsync();
+            const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
             this.getWeather(latitude, longitude);
             //Send to API and get Weather
         } catch (error) {
@@ -31,12 +42,12 @@ export default class extends React.Component {
         }
     }
 
-    componentDidMount(){
-        this,this.getLocation();
+    componentDidMount() {
+        this, this.getLocation();
     }
 
-    render(){
-        const {isLoading, temp} = this.state;
-        return isLoading ? <Loading/> : <Weather temp={Math.round(temp)}/>;
+    render() {
+        const { isLoading, temp, condition } = this.state;
+        return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
     }
 }
--- Weather.js
+++ Weather.js
@@ -1,23 +1,61 @@
 import React from "react";
-import {StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({temp}){
+export default function Weather({ temp }) {
     return (
-    <View style={styles.container}>
-        <Text>{temp}</Text>
-    </View>
+        <View style={styles.container}>
+            <View style={styles.topHalfContainer}>
+                <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+                <Text style={styles.temp}>{temp}°</Text>
+            </View>
+            <View style={styles.bottomHalfContainer}>
+
+            </View>
+        </View>
     );
 }
 
 Weather.propTypes = {
-    temp: PropTypes.number.isRequired
-}
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Mist",
+        "Haze",
+        "Smoke",
+        "Dust",
+        "Fog",
+        "Sand",
+        "Ash",
+        "Squall",
+        "Tornado",
+    ]).isRequired,
+};
 
 const styles = StyleSheet.create({
-    container : {
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    topHalfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    bottomHalfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp: {
+        fontSize: 42
     }
-})
+});
